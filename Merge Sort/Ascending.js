6c9237187e0e9d64ed0c2680cba7f4785facd3b4
Eshan <3shan.bd@gmail.com>
2023-02-08 23:34:51 +0600

    descending

--- Merge Sort/Ascending.js
+++ Merge Sort/Ascending.js
@@ -22,3 +22,60 @@ function merge(leftArr, rightArr) {
 }
 
 console.log(mergeSort([-7, 3, 1, 20, 7, 45, -2, 44, 5]));
+
+// Another way of sorting
+
+function merge2(arr, f, l, m) {
+
+    let n1 = m - f + 1;
+    let n2 = l - m;
+    
+    const leftArr = [];
+    const rightArr = [];
+
+    //copy two array from main array
+    for (let i = 0; i < n1; i++){
+        leftArr[i]= arr[i]
+    }
+    for (let j = 0 ; j < n2 ; j++){
+        rightArr[j]= arr[m+j]
+    }
+    
+    let i = 0;
+    let j = 0;
+    let k = f;
+    while (i < n1 && j < m + n2) {
+        if (leftArr[i] < rightArr[j]) {
+            arr[k] = leftArr[i]
+            i++
+        }
+        else {
+            arr[k] = rightArr[j]
+            j++
+        }
+    }
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+    return arr;
+}
+
+
+function mergeSort2(arr, l, r) {
+  if (l >= r) {
+    return; //returns recursively
+  }
+  var m = l + parseInt((r - l) / 2);
+  mergeSort2(arr, l, m);
+  mergeSort2(arr, m + 1, r);
+    merge2(arr, l, r, m);
+}
+
+console.log(mergeSort2([-7, 3, 1, 20, 7, 45, -2, 44, 5], 0, 9));
